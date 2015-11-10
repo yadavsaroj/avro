f5b3d5b0a2b9bcdaba9aa090897f4d41471eca26
Saroj Yadav <syadav@zendesk.com>
2015-11-09 17:11:48 -0800

    Minor cosmetic change

--- lang/ruby/lib/avro/json_io.rb
+++ lang/ruby/lib/avro/json_io.rb
@@ -91,9 +91,9 @@ module Avro
       end
 
       def read_record(writers_schema, readers_schema, decoder)
-        readers_fields_hash = readers_schema.fields_hash
         read_record = {}
 
+        readers_fields_hash = readers_schema.fields_hash
         writers_schema.fields.each do |field|
           if readers_field = readers_fields_hash[field.name]
             field_data = decoder.data ? decoder.data[field.name] : nil
@@ -106,9 +106,8 @@ module Avro
 
         # fill in the default values
         if readers_fields_hash.size > read_record.size
-          writers_fields_hash = writers_schema.fields_hash
           readers_fields_hash.each do |field_name, field|
-            unless writers_fields_hash.has_key? field_name
+            unless writers_schema.fields_hash.has_key? field_name
               if !field.default.nil?
                 field_val = read_default_value(field.type, field.default)
                 read_record[field.name] = field_val
